3867205f703d8595a410ea3403f198f430af0594
garrsaun253 <garrsaun253@SCS-D-360QHY>
2019-04-12 09:30:43 -0400

    Better image getting

--- XMLWeather/ForecastScreen.cs
+++ XMLWeather/ForecastScreen.cs
@@ -29,95 +29,48 @@ namespace XMLWeather
             date1.Text = DateTime.Now.ToString("dddd");
             min1.Text = "Min: " + Convert.ToDouble(Form1.days[0].tempLow).ToString("0.") + "°C";
             max1.Text = "Max: " + Convert.ToDouble(Form1.days[0].tempHigh).ToString("0.") + "°C";
-            #region Image
-            //Thunder
-            if (code1 > 199 && code1 < 300)
-            {
-                conditions1.Image = XMLWeather.Properties.Resources.rain;
-            }
-            //Rain
-            if (code1 > 299 && code1 < 400)
-            {
-                conditions1.Image = XMLWeather.Properties.Resources.rain;
-            }
-            //Light Rain
-            else if (code1 > 499 && code1 < 600)
-            {
-                conditions1.Image = XMLWeather.Properties.Resources.light_rain;
-            }
-            //Light Rain
-            else if (code1 > 599 && code1 < 700)
-            {
-                conditions1.Image = XMLWeather.Properties.Resources.snow;
-            }
-            //Clear or Cloudy
-            else
-            {
-                conditions1.Image = XMLWeather.Properties.Resources.clear;
-            }
-            #endregion
+            conditions1.Image = GetSymbol(0);
 
             date2.Text = DateTime.Now.AddDays(1).DayOfWeek.ToString();
             min2.Text = "Min: " + Convert.ToDouble(Form1.days[1].tempLow).ToString("0.") + "°C";
             max2.Text = "Max: " + Convert.ToDouble(Form1.days[1].tempHigh).ToString("0.") + "°C";
-            #region Second Image
-            //Thunder
-            if (code2 > 199 && code2 < 300)
-            {
-                conditions2.Image = XMLWeather.Properties.Resources.rain;
-            }
-            //Rain
-            if (code2 > 299 && code2 < 400)
-            {
-                conditions2.Image = XMLWeather.Properties.Resources.rain;
-            }
-            //Light Rain
-            else if (code2 > 499 && code2 < 600)
-            {
-                conditions2.Image = XMLWeather.Properties.Resources.light_rain;
-            }
-            //Light Rain
-            else if (code2 > 599 && code2 < 700)
-            {
-                conditions2.Image = XMLWeather.Properties.Resources.snow;
-            }
-            //Clear or Cloudy
-            else
-            {
-                conditions2.Image = XMLWeather.Properties.Resources.clear;
-            }
-            #endregion
+            conditions2.Image = GetSymbol(1);
 
             date3.Text = DateTime.Now.AddDays(2).DayOfWeek.ToString();
             min3.Text = "Min: " + Convert.ToDouble(Form1.days[2].tempLow).ToString("0.") + "°C";
             max3.Text = "Max: " + Convert.ToDouble(Form1.days[2].tempHigh).ToString("0.") + "°C";
-            #region Third Image
+            conditions3.Image = GetSymbol(2);
+        }
+
+        public Image GetSymbol(int d)
+        {
+            double code = Convert.ToDouble(Form1.days[d].conditionNumber);
+
             //Thunder
-            if (code3 > 199 && code3 < 300)
+            if (code > 199 && code < 300)
             {
-                conditions3.Image = XMLWeather.Properties.Resources.rain;
+                return XMLWeather.Properties.Resources.rain;
             }
             //Rain
-            if (code3 > 299 && code3 < 400)
+            if (code > 299 && code < 400)
             {
-                conditions3.Image = XMLWeather.Properties.Resources.rain;
+                return XMLWeather.Properties.Resources.rain;
             }
             //Light Rain
-            else if (code3 > 499 && code3 < 600)
+            else if (code > 499 && code < 600)
             {
-                conditions3.Image = XMLWeather.Properties.Resources.light_rain;
+                return XMLWeather.Properties.Resources.light_rain;
             }
             //Light Rain
-            else if (code3 > 599 && code3 < 700)
+            else if (code > 599 && code < 700)
             {
-                conditions3.Image = XMLWeather.Properties.Resources.snow;
+                return XMLWeather.Properties.Resources.snow;
             }
             //Clear or Cloudy
             else
             {
-                conditions3.Image = XMLWeather.Properties.Resources.clear;
+                return XMLWeather.Properties.Resources.clear;
             }
-            #endregion
         }
 
         private void label3_Click(object sender, EventArgs e)
